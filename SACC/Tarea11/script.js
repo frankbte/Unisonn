52b0aba21eab6df7a236b360cd6d523984ede9b9
frank <frankyanez2002@gmail.com>
2024-09-25 09:09:44 -0700

    Tarea11 sacc

--- SACC/Tarea11/script.js
+++ SACC/Tarea11/script.js
@@ -1,29 +1,46 @@
 const textInput = document.getElementById("textInput");
 const submitButton = document.getElementById("submitButton");
+const tabla = document.getElementById("tabla");
 
-let arreglo = [];
+let valorUsuario = [];
 
 textInput.addEventListener("input", function () {
   submitButton.disabled = textInput.value.length === 0;
 });
 
-function agregarElemento(inputValue) {
-  const li = document.createElement("li");
-  li.textContent = inputValue;
-  document.getElementById("historial").appendChild(li);
-}
-
 submitButton.addEventListener("click", function () {
-  agregarInput(textInput);
+  agregarInput();
 });
 
-function agregarInput(input) {
+function agregarInput() {
   const inputValue = textInput.value.trim();
   if (inputValue) {
-    arreglo.push(inputValue);
+    valorUsuario.push(inputValue);
     textInput.value = "";
     submitButton.disabled = true;
 
-    agregarElemento(inputValue);
+    actualizarTabla();
+  }
+}
+
+function actualizarTabla() {
+  tabla.innerHTML = "";
+
+  const numElementos = valorUsuario.length;
+  const tamañoTabla = Math.ceil(Math.sqrt(numElementos));
+
+  let indice = 0;
+
+  for (let i = 0; i < tamañoTabla; i++) {
+    const fila = document.createElement("tr");
+    for (let j = 0; j < tamañoTabla; j++) {
+      const celda = document.createElement("td");
+      if (indice < numElementos) {
+        celda.textContent = valorUsuario[indice];
+        indice++;
+      }
+      fila.appendChild(celda);
+    }
+    tabla.appendChild(fila);
   }
 }
